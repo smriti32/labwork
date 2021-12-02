5cad7a9199afeecaeb7abdc1ce5cab68379b2507
smriti32 <sharmaa.smriti028@gmail.com>
2021-12-02 12:08:48 -0800

    lab

---
+++ lab2.py
@@ -0,0 +1,9 @@
+# h=int(input("enter height of triangle"))
+# b=int(input("enter base of triangle"))
+# area=h*b/2
+# print(area)
+# print(f"the area of triangles is {area}.cm\u00b2")
+
+l=int(input("enter length of cube"))
+volumn=l**3
+print(f"the volume of cube is {volumn}.cm\u00b3")
---
+++ lab3.py
@@ -0,0 +1,22 @@
+# sum of first 10 positive integer using list or range
+# using list
+# a=[1,2,3,4,5,6,7,8,9,10]
+# sum=sum(a)
+# print(sum)
+# using range
+# a=range(1,11)
+# sum=sum(a)
+# print(sum)
+# area of circle
+# r=int(input("Enter a redius :  ")) 
+# areaOfCircle = (r ** 2) * 22/7
+# print(areaOfCircle) 
+# print(f"the are of circle is  {areaOfCircle} cm\u00b2") 
+# sum of positive odd integer number till 10
+# numbers = range(1,11,2)
+# sum = sum(numbers)
+# print(sum) 
+# sum of positive even integers numbers till 10
+numbers = range(0,11,2)
+sum = sum(numbers)
+print(sum) 
