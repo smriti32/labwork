7143afaabeb7d9f2e62d093c7d18c5be370f9c9a
smriti32 <sharmaa.smriti028@gmail.com>
2021-12-06 12:09:31 -0800

    assignmnet

---
+++ day2/lab1.py
@@ -0,0 +1,35 @@
+# q.no1
+# convert seconds to day, hours, minutes and seconds
+# seconds=864600
+# hr=864600/60**2
+# print(hr)
+
+# q.no2
+# s=int(input("enter the valur for second:"))
+# day=(((s/60)/60)/24)
+# print(f"total day of given second:{day}")
+# hour=((s/60)/60)
+# print(f"total hours of given seconds:{hour}")
+# minute=(s/60)
+# print(f"total minutes for given seconds:{minute}")
+
+# qno.3
+# mass=int(input("enter mass of person:"))
+# height=int(input("enter height of person:"))
+# bmi=mass/height
+# print(f"bmi of person:{bmi}.kg\m\u00b2") 
+
+# a=int(input("Enter time in minute:"))
+# hours= a//60
+# minutes= a%60
+# print(hours,minutes)
+
+# no_students = int(input("Total number of students:"))
+# no_apple = int(input("Total number of apple:"))
+# print( no_apple // no_students )
+# print( no_apple % no_students )
+
+a=int(input())
+b=int(input())
+c=int(input())
+a(a//2+b//2+c//2+a%2+b%2+c%2)
---
+++ day3/lab1.py
@@ -0,0 +1,60 @@
+#Check whether 5 is in list of first 5 natural numbers or not. Hint: List => [1,2,3,4,5] 
+# a=(1,2,3,4,5)
+# if 5==a:
+#     print("yes")
+# else:
+#     print("no")
+
+# question2
+# a=int(input("Marks of Maths:"))
+# b=int(input("Marks of Science:"))
+# c=int(input("Marks of English:"))
+# d=int(input("Marks of Computer:"))
+# Total_marks=(a+b+c+d)/4
+# if Total_marks>100:
+#     print("Envalid")
+# elif Total_marks>=70:
+#     print("Distinction")
+# elif Total_marks>=60:
+#     print("First")
+# elif Total_marks>=40:
+#     print("Pass")
+# else:
+#     print("Fail")
+
+# question 3
+# a=int(input("Enter the number"))
+# if(a%2)==0:
+#     print("{0} is a even number".format(a))
+# else:
+#     print("{0} is a odd number".format(a))
+
+# question4
+# a=int(input("Enter the first number:"))
+# b=int(input("Enter the second number:"))
+# c=int(input("Enter the third number:"))
+# if a<=b and a<=c:
+#     print("First number is the smallest")
+# elif b<=a and b<=c:
+#     print("Second number is the smallest")
+# elif c<=a and c<=a:
+#     print("Third number is the smallest")
+
+# question 5
+# a=int(input("Number:"))
+# if a>=1:
+#     print("True")
+# elif a<=-1:
+#     print("False")
+# else:
+#     print("Zero")
+
+# question 6
+# a=int(input("The given number's last number is "))
+# last_digit= a%10
+# print("The last digit in the given number %d = %d"%(a,last_digit))
+
+# question 7
+a=float(input("Enter the number:"))
+print(a-int(a))
+
